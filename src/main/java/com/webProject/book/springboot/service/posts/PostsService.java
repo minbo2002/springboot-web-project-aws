d841b7bf097e5c40028adde0b1ece1a3c1d061a7
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-06-09 22:15:30 +0900

    1. 등록 Controller, Dto, Service 클래스 생성 2. 등록 JUnit 테스트 진행

---
+++ src/main/java/com/webProject/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.webProject.book.springboot.service.posts;
+
+import com.webProject.book.springboot.domain.posts.PostsRepository;
+import com.webProject.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
