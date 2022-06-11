eb9254c3dde0e1eff630f682db87e75a9f606812
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-06-11 10:44:15 +0900

    1. Mustache 설정 및 index, footer, header 추가 2. 글 등록, 수정, 삭제 기능 추가

--- src/main/java/com/webProject/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/webProject/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.webProject.book.springboot.service.posts;
 
 import com.webProject.book.springboot.domain.posts.Posts;
 import com.webProject.book.springboot.domain.posts.PostsRepository;
+import com.webProject.book.springboot.web.dto.PostsListResponseDto;
 import com.webProject.book.springboot.web.dto.PostsResponseDto;
 import com.webProject.book.springboot.web.dto.PostsSaveRequestDto;
 import com.webProject.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,18 +10,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    // 등록
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
 
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    // 수정
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
 
@@ -40,5 +46,26 @@ public class PostsService {
                         -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    // 목록
+    @Transactional(readOnly = true)  // transaction 범위유지 및 조회기능만 남김.  --> 조회속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)  // .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+
+             // postsRepository 결과로 넘어온 Posts의 stream을
+             // map을 통해 PostsListResponseDto 변환후 --> List로 반환
+    }
+
+    @Transactional
+    public void delete(Long id) {
+
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
---
+++ src/main/java/com/webProject/book/springboot/web/IndexController.java
@@ -0,0 +1,45 @@
+package com.webProject.book.springboot.web;
+
+import com.webProject.book.springboot.service.posts.PostsService;
+import com.webProject.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    // 목록화면
+    @GetMapping("/")
+    public String index(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        return "index";
+    }
+
+    // 등록화면
+    @GetMapping("/posts/save")
+    public String postsSave() {
+
+        return "posts-save";
+    }
+
+    // 수정화면
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
+
+
+}
